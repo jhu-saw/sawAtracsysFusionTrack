1df89244f038eda90254582fdbefb6de07d01fb3
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-02-24 01:47:57 -0500

    Add isocenter calculations

--- ros/tool_maker.py
+++ ros/tool_maker.py
@@ -18,6 +18,7 @@ import sys
 import argparse
 import rospy
 import numpy
+import json
 import scipy.spatial
 
 import geometry_msgs.msg
@@ -27,44 +28,75 @@ collecting = False
 number_of_markers = 0
 records = []
 
+# Arbitrary number to make sure we have enough records to average out noise etc.
+minimum_records_required = 10
+
 if sys.version_info.major < 3:
     input = raw_input
 
+
 def pose_array_callback(msg):
     global records
-    # do something only if we are recording
-    if collecting:
-        # make sure the number of poses matches the number of expected markers
-        if (len(msg.poses) == number_of_markers):
-            record = []
-            for marker in range(number_of_markers):
-                record.append([msg.poses[marker].position.x,
-                               msg.poses[marker].position.y,
-                               msg.poses[marker].position.z])
-            records.append(record)
-            sys.stdout.write('\rNumber of samples collected: %i' % len(records))
-            sys.stdout.flush()
+
+    # exit if not recording marker pose messages
+    if not collecting:
+        return
+
+    # make sure the number of poses matches the number of expected markers
+    if len(msg.poses) == number_of_markers:
+        record = []
+        for marker in range(number_of_markers):
+            record.append(
+                [
+                    msg.poses[marker].position.x,
+                    msg.poses[marker].position.y,
+                    msg.poses[marker].position.z,
+                ]
+            )
+        records.append(record)
+        sys.stdout.write("\rNumber of samples collected: %i" % len(records))
+        sys.stdout.flush()
+
 
 # ros init node so we can use default ros arguments (e.g. __ns:= for namespace)
-rospy.init_node('tool_maker', anonymous=True)
+rospy.init_node("tool_maker", anonymous=True)
 # strip ros arguments
 argv = rospy.myargv(argv=sys.argv)
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--topic', type = str, required = True,
-                    help = 'topic to use to receive PoseArray without namespace.  Use __ns:= to specify the namespace')
-parser.add_argument('-n', '--number-of-markers', type = int, choices = range(3, 10), required = True,
-                    help = 'number of markers on the tool.  This will use to filter messages with incorrect number of markers')
+parser.add_argument(
+    "-t",
+    "--topic",
+    type=str,
+    required=True,
+    help="topic to use to receive PoseArray without namespace.  Use __ns:= to specify the namespace",
+)
+parser.add_argument(
+    "-n",
+    "--number-of-markers",
+    type=int,
+    choices=range(3, 10),
+    required=True,
+    help="number of markers on the tool.  This will use to filter messages with incorrect number of markers",
+)
+parser.add_argument(
+    "-p",
+    "--planar",
+    action="store_true",
+    help="indicates all markers lie in a plane",
+)
+parser.add_argument("-o", "--output", type=str, required=True, help="output file name")
 
-args = parser.parse_args(argv[1:]) # skip argv[0], script name
+
+args = parser.parse_args(argv[1:])  # skip argv[0], script name
+number_of_markers = args.number_of_markers
+print("Planar: %s" % args.planar)
 
 # create the callback that will collect data
-pose_array_subscriber = rospy.Subscriber(args.topic,
-                                         geometry_msgs.msg.PoseArray,
-                                         pose_array_callback)
-
-number_of_markers = args.number_of_markers
+pose_array_subscriber = rospy.Subscriber(
+    args.topic, geometry_msgs.msg.PoseArray, pose_array_callback
+)
 
 input("Press Enter to start collection using topic %s" % args.topic)
 print("Collection started\nPress Enter to stop")
@@ -75,37 +107,49 @@ collecting = False
 
 nb_records = len(records)
 
-if nb_records < 10:
-     sys.exit("Not enough records (10 minimum)") # this is totally arbitrary
+if nb_records < minimum_records_required:
+    sys.exit("Not enough records ({} minimum)".format(minimum_records_required))
 
 # now the fun part, each record has n poses but we don't know if they are sorted by markers
 
 # create n lists to store the pose of each marker based on distance, using the last record as reference
-reference = records.pop()
+reference = records[-1]
 
 # create a records with markers sorted by proximity to reference order
-sorted_records = []
-sorted_records.append(reference)
+sorted_records = [
+    [
+        record[scipy.spatial.distance.cdist([reference_marker], record).argmin()]
+        for reference_marker in reference
+    ]
+    for record in records
+]
 
-# iterate through rest of list
-for record in records:
-    correspondence = [] # index of closest reference
-    # find correspondence
-    for marker in record:
-        # find closest reference
-        min = 100000.0 # arbitrary high
-        closest_reference = -1
-        for index_reference in range(0, len(reference)):
-            distance = scipy.spatial.distance.euclidean(marker, reference[index_reference])
-            if distance < min:
-                min = distance
-                closest_to_reference = index_reference
-        correspondence.append(closest_to_reference)
-    # create sorted record
-    sorted_record = record # just to make sure we have the correct size
-    for index in correspondence:
-        sorted_record[correspondence[index]] = record[index]
-    sorted_records.append(sorted_record)
+# average position of each marker
+averaged_marker_poses = numpy.mean(sorted_records, axis=0)
+# center (average) of individual average marker positions
+isocenter = numpy.mean(averaged_marker_poses, axis=0)
+# center coordinate system on isocenter
+points = averaged_marker_poses - isocenter
 
+# Project markers to best-fit plane
+if args.planar:
+    pass # TODO
+    # # Minimizing Ax gives best-fit plane through origin (isocenter)
+    # plane = numpy.linalg.lstsq(points, numpy.zeros(len(points)), rcond=None)
+    # # Project onto plane
+    # points = points - (points*plane/numpy.dot(plane, plane))*plane
+    # transformation = numpy.array(plane, )
 
-print("------------- to do, compute sum/average for each value, then isocenter and finally subtract isocenter, then create ini or json file")
+fiducials = [
+    {"x": x, "y": y, "z": z}
+    for [x, y, z] in points
+]
+
+data = {
+    "count": number_of_markers,
+    "fiducials": fiducials,
+}
+
+with open(args.output, "w") as f:
+    json.dump(data, f, indent=4)
+    f.write("\n")
